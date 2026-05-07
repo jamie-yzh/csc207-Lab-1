c8b2a7e79ca1ec2e355c73ddacff6ea61997af6e
Jamie Ye <jamie.yzh@gmail.com>
2026-05-07 19:24:58 -0400

    Add Main.java that calls FizzBuzz.main and edit FizzBuzz.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,12 +8,12 @@ class FizzBuzz {
         int i = 1;
         while (i < 100) {
 
-            DoFizzBuzz(i);
+            doFizzBuzz(i);
             i++;
         }
     }
 
-    private static void DoFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+class Main {
+
+    public static void main(String[] args) {
+        FizzBuzz.main(args);
+        FizzBuzz.doFizzBuzz(5);
+    }
+}
+
+
+
+
